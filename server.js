21612d0765aabac29a4e5e0d113d92f554239e4a
rohithpaul0304 <34884172+rohithpaul0304@users.noreply.github.com>
2018-02-19 21:59:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send('article three is requested and will be served');
 });
 app.get('/article-two', function(req, res){
     res.send('article two is requested and will be served');
