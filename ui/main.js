c346f2b1207825ef15d600fea048b3f1c87c24e9
rohithpaul0304 <34884172+rohithpaul0304@users.noreply.github.com>
2018-02-20 22:22:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,11 @@
 console.log('Loaded!');
+
 //Change the text of the main-text div
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
+var element = document.getElementById('main-text');
+element.innerHTML = 'New value';
+
 //Move the image
 var img=document.getElementById('madi');
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    img.style.marginLeft = '100px';
 };
