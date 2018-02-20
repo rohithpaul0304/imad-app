e8e39071978c559f939c47500c88b3f283c3e2cf
rohithpaul0304 <34884172+rohithpaul0304@users.noreply.github.com>
2018-02-20 21:45:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,48 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-var htmlTemplate=`
-<html>
-<head>
-<title>
-${title}
-</title>
-</head>
-<body>
-<div>
-<a href="/">HOME</a>
-</div>
-<hr/>
-<h3>
-${heading}
-</h3>
-<div>
-${date}
-</div>
-<div>
-${content}
-</div>
-</body>
-</html>
-`;
-return htmlTemplate;
-}
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
-});
-app.get('/article-one', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -55,6 +15,15 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
 });
 
 
