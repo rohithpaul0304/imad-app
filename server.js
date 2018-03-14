846495b88fc1532deddcca85e27cb4ea97024bc7
rohithpaul0304 <34884172+rohithpaul0304@users.noreply.github.com>
2018-03-14 21:35:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var counter = 0;
-app.get('/counter', function (req, res){
-   counter = counter + 1;
-   res.send(counter.toString());
-});
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
