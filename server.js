f20b805eb6dd75c9b94121760dd9419d9f4e7f04
rohithpaul0304 <34884172+rohithpaul0304@users.noreply.github.com>
2018-03-19 23:12:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');//output logs
 var path = require('path');
-
+var crypto = require('crypto');
 var config = {
     user: 'rohithp0304',
     database: 'rohithp0304',
@@ -124,6 +124,17 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+function hash (input) {
+    //how do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+    
 });
 app.get('/ui/Portrait.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Portrait.jpg'));
